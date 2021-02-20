0f1bb0665d7e3a965cb29dbc93b1d309f4c7f0f0
zethdeluna <zethdeluna@users.noreply.github.com>
2021-02-20 04:31:49 -0800

    added load state check for name fade-in

--- src/Homepage.js
+++ src/Homepage.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import background_ink from './background_ink.mp4';
 
 class Homepage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            animate: true
+        }
+    }
+    DidMount() {
+        // check for token (if page has loaded or not)
+        // if the page has not loaded, set state animate to true
+        if (window.sessionStorage.getItem("firstLoadDone") === null) {
+            this.setState({
+                animate: true
+            });
+            // set the token to be found the following times
+            window.sessionStorage.setItem("firstLoadDone", 1);
+        } else {
+            this.setState({
+                animate: false
+            });
+        }
+    }
     render() {
         return (
             <div id="homepage">
@@ -10,7 +31,7 @@ class Homepage extends Component {
                     <source src={background_ink} type="video/mp4" />
                 </video>
                 <div id="navbar">
-                    <h1 id="myName">Zeth De Luna</h1>
+                    <h1 id="myName" className={this.state.animate ? "fade-in" : ""}>Zeth De Luna</h1>
                 </div>
             </div>
         )
