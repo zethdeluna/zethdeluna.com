1715550890968c2977081b39a160b5c750696329
zethdeluna <zethdeluna@users.noreply.github.com>
2021-02-21 00:03:16 -0800

    fixed navbar, added some text

--- src/Homepage.js
+++ src/Homepage.js
@@ -5,7 +5,8 @@ class Homepage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            animate: true
+            // animate property for deciding how tot animate an element
+            animate: true 
         }
     }
     DidMount() {
@@ -30,13 +31,19 @@ class Homepage extends Component {
                 <video id="bkgdVideo" autoPlay loop muted>
                     <source src={background_ink} type="video/mp4" />
                 </video>
-                <div id="navbar">
-                    <h1 id="myName" className={this.state.animate ? "fade-in" : ""}>Zeth De Luna</h1>
+                {/* navigation */}
+                <div id="navbar" className={this.state.animate ? "fade-in" : ""}>
+                    <h1 id="myName"><a href="#homepage">Zeth De Luna</a></h1>
+                    <ul>
+                        <li><a href="https://github.com/zethdeluna" target="_blank" rel="noreferrer"><i class="fab fa-github-alt"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/zethdeluna/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin-in"></i></a></li>
+                    </ul>
                 </div>
+                {/* header text */}
                 <div id="myOpening" className={this.state.animate ? "slide-top": ""}>
                     <h2>Hi,<br/> my name is <span>Zeth</span>,<br/>an aspiring <span>web developer</span>.</h2>
                 </div>
-                <div id="skills">
+                <div id="skills" className={this.state.animate ? "slide-left": ""}>
                     <h3>
                         &#60;div&#62;<br/>
                         &#160;&#160; html <br/>
