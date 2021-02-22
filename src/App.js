e2f3b79d5e53303e33592e7e7bb6247bf1903c9d
zethdeluna <zethdeluna@users.noreply.github.com>
2021-02-21 18:42:38 -0800

    Added custom cursor component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './App.scss';
 import Homepage from './Homepage';
+import Cursor from './Cursor';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Homepage/>
-      </div>
-    )
-  }
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <Cursor/>
+//         <Homepage/>
+//       </div>
+//     )
+//   }
+// }
+
+function App() {
+  return (
+    <div className="App">
+      <Cursor/>
+      <Homepage/>
+    </div>
+  )
 }
 
 export default App;
