598083a61cbdfeafb2a2d0db6a72ecb2f72f8e9e
zethdeluna <zethdeluna@users.noreply.github.com>
2021-02-21 23:14:44 -0800

    full update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import './App.scss';
 import Homepage from './Homepage';
-import Cursor from './Cursor';
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <Cursor/>
-//         <Homepage/>
-//       </div>
-//     )
-//   }
-// }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        xMain: 0,
+        yMain: 0,
+        xTrailing: 0,
+        yTrailing: 0
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <Cursor/>
-      <Homepage/>
-    </div>
-  )
+  handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+
+      this.setState({
+          xMain: clientX,
+          yMain: clientY
+      }, () => {
+          setTimeout(() => {
+              this.setState({
+                  xTrailing: clientX,
+                  yTrailing: clientY
+              });
+          }, 120);
+      })
+  }
+
+  render() {
+    const {
+      xMain,
+      yMain,
+      xTrailing,
+      yTrailing
+    } = this.state;
+    return (
+      <div className="App" onMouseMove={e => {this.handleMouseMove(e)}}>
+        <div className="cursors">
+          <div className="cursor fade-in" style={{left: xMain, top: yMain}}/>
+          <div className="cursor fade-in" style={{left: xTrailing, top: yTrailing}}/>
+        </div>
+        <Homepage/>
+      </div>
+    )
+  }
 }
 
 export default App;
