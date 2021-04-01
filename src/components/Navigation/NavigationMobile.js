59d501cf9b78a3c12f8f37423eb9d5cc1d80b238
zethdeluna <zethdeluna@users.noreply.github.com>
2021-04-01 10:57:57 -0700

    added projects link in navbar

--- src/components/Navigation/NavigationMobile.js
+++ src/components/Navigation/NavigationMobile.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './NavigationMobile.scss';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 function NavigationMobile() {
     return (
         <div id="navbarMobile">
-            <h1 id="myName"><a href="https://zethdeluna.com/">Zeth De Luna</a></h1>
+            <h1 id="myName"><AnchorLink className="anchorMobile" href="#helloMobileContainer">Zeth De Luna</AnchorLink></h1>
             <ul>
+                <li>
+                    <AnchorLink className="anchorMobile projects" href="#projectsMobileContainer">projects</AnchorLink>
+                </li>
                 <li><a href="https://github.com/zethdeluna" target="_blank" rel="noreferrer"><i className="fab fa-github-alt"></i></a></li>
                 <li><a href="https://www.linkedin.com/in/zethdeluna/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a></li>
             </ul>
