e0aebd460fd5275df4b73227c17730165113dc00
zethdeluna <zethdeluna@users.noreply.github.com>
2021-04-01 10:45:29 -0700

    added projects link to navbar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './Navigation.scss';
+import AnchorLink from 'react-anchor-link-smooth-scroll';
 
 function Navigation() {
     return (
         <div>
             {/* navigation */}
             <div id="navbar">
-                <h1 id="myName"><a href="https://zethdeluna.com/">Zeth De Luna</a></h1>
+                <h1 id="myName"><AnchorLink className="AnchorLink" href="#helloContainer">Zeth De Luna</AnchorLink></h1>
                 {/* <button id="myName">Zeth De Luna</button> */}
                 <ul>
+                    <li>
+                        <AnchorLink className="AnchorLink" href="#projectsContainer">projects</AnchorLink>
+                    </li>
                     <li><a href="https://github.com/zethdeluna" target="_blank" rel="noreferrer"><i className="fab fa-github-alt"></i></a></li>
                     <li><a href="https://www.linkedin.com/in/zethdeluna/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a></li>
                 </ul>
