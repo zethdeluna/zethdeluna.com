794ce804f6a46efa005b76112d8be8157e584ee5
zethdeluna <zethdeluna@users.noreply.github.com>
2021-04-01 10:44:08 -0700

    updated method for displaying website version (desktop vs mobile)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect, createContext, useContext } from 'react';
+import React, { useState, useEffect, createContext, useContext } from 'react';
 import './App.scss';
 import { 
   Navigation, NavigationMobile, 
@@ -81,66 +81,5 @@ function App() {
     </div>
   )
 }
-
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//         // initial state of window size
-//         width: 0,
-//     };
-//     this.updateDimensions = this.updateDimensions.bind(this);
-//   }
-
-//   componentDidMount() {
-//     // window size
-//     this.updateDimensions();
-//     window.addEventListener("resize", this.updateDimensions);
-//   }
-//   componentWillUnmount() {
-//     // window size
-//     window.removeEventListener("resize", this.updateDimensions);
-//   }
-
-//   // track window size and update state
-//   updateDimensions() {
-//     let windowWidth = typeof window !== "undefined" ? window.innerWidth : 0;
-//     this.setState({
-//       width: windowWidth,
-//     });
-//   }
-
-//   render() {
-//     const width = this.state.width;
-//     let navigation, hello, homepage, about, projects, contact;
-
-//     if (width < 780) {
-//       navigation = <NavigationMobile/>;
-//       hello =<HelloMobile/>;
-//       homepage = <HomepageMobile/>;
-//       about = <AboutMobile/>;
-//       projects = <ProjectsMobile/>;
-//       contact = <ContactMobile/>;
-//     } else {
-//       navigation = <Navigation/>;
-//       hello = <Hello/>;
-//       homepage = <Homepage/>;
-//       about = <About/>;
-//       projects = <Projects/>;
-//       contact = <Contact/>;
-//     }
-
-//     return (
-//       <div className="App">
-//           {navigation}
-//           {hello}
-//           {homepage}
-//           {about}
-//           {projects}
-//           {contact}
-//       </div>
-//     )
-//   }
-// }
 
 export default App;
