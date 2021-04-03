61a6ed3d1a2f584b73f2d751b0af3d921ea4af4b
zethdeluna <zethdeluna@users.noreply.github.com>
2021-04-03 00:48:02 -0700

    added Frontend Mentor link

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Navigation.scss';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import frontendMentor from '../../images/frontend-mentor.png';
 
 function Navigation() {
     return (
@@ -8,13 +9,13 @@ function Navigation() {
             {/* navigation */}
             <div id="navbar">
                 <h1 id="myName"><AnchorLink className="AnchorLink" href="#helloContainer">Zeth De Luna</AnchorLink></h1>
-                {/* <button id="myName">Zeth De Luna</button> */}
                 <ul>
                     <li>
                         <AnchorLink className="AnchorLink" href="#projectsContainer">projects</AnchorLink>
                     </li>
                     <li><a href="https://github.com/zethdeluna" target="_blank" rel="noreferrer"><i className="fab fa-github-alt"></i></a></li>
                     <li><a href="https://www.linkedin.com/in/zethdeluna/" target="_blank" rel="noreferrer"><i className="fab fa-linkedin-in"></i></a></li>
+                    <li><a href="https://www.frontendmentor.io/profile/zethdeluna" target="_blank" rel="noreferrer"><img id="frontendMentorLogo" src={frontendMentor} alt="Frontend Mentor logo"/></a></li>
                 </ul>
             </div>
         </div>
